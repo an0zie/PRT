33eda36b8e4aac82ab244837ebf8b3e7e2ab610f
an0zie <michael1.anozie@gmail.com>
2024-10-06 19:50:34 +0100

    Third commit

---
+++ Afghanistan.java
@@ -0,0 +1,14 @@
+public class 
+{
+    public static void main(String[]args)
+    {
+        
+
+        for (int i = 0; i <= 5; i++);
+        {
+            
+        System.out.println("Numbers: "+i+1);
+
+        }
+    }
+}
