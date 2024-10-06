e695a339c69148acc426345e31b156612c79fa2d
an0zie <michael1.anozie@gmail.com>
2024-10-06 19:10:30 +0100

    Second commit

---
+++ csaba.java
@@ -0,0 +1,14 @@
+public class 
+{
+    public static void main(String[]args)
+    {
+        
+
+        for (int i = 0; i <= 4; i++);
+        {
+            
+        System.out.println("Alphabet: "+i+1);
+
+        }
+    }
+}
